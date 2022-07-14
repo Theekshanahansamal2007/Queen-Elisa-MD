cadbe62a00663f7aa5cfeebbe3b12d30f8e2c7e2
Theekshanahansamal2007 <94382409+Theekshanahansamal2007@users.noreply.github.com>
2022-07-15 01:21:36 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,19 +1,15 @@
 /*
- █▀▀█ █░▒█ █▀▀▀ █▀▀▀ █▄░▒█       █▀▀▀ █░░░ ▀█▀ █▀▀▀█ █▀▀█      
- █░▒█ █░▒█ █▀▀▀ █▀▀▀ █▒█▒█       █▀▀▀ █░░░ ░█░ ▀▀▀▄▄ █▄▄█      
- ▀▀█▄ ▀▄▄▀ █▄▄▄ █▄▄▄ █░░▀█       █▄▄▄ █▄▄█ ▄█▄ █▄▄▄█ █░▒█      
+ 
+ 𝕚 𝕒𝕞 𝗧𝗜𝗞𝗞𝗔 ᵇᵒᵗ ʕ•ᴥ•ʔ
 
 
- █░░▒█   ▄█░
- ▒█▒█░   ░█░
- ░▀▄▀░   ▄█▄
- 
- 𝙌𝙪𝙚𝙚𝙣 𝙀𝙡𝙞𝙨𝙖 𝙈𝘿 𝘽𝙮 𝘿𝙖𝙧𝙠 𝙈𝙖𝙠𝙚𝙧 
- 𝙋𝙪𝙗𝙡𝙞𝙘 𝘿𝙖𝙩𝙚 - 2022 / 07 / 03 
- 𝙏𝙝𝙖𝙣𝙠𝙨 𝙏𝙤 𝙎𝙘𝙧𝙞𝙥𝙩 𝙁𝙧𝙤 𝙂𝙤𝙟𝙤 𝙎𝙖𝙩𝙤𝙧𝙪
- 
- 𝙏𝙝𝙖𝙣𝙠𝙨 𝙏𝙤
-           𝘼𝙡𝙡 𝙃𝙚𝙡𝙥𝙚𝙧𝙨 💞
+
+
+
+𝘥𝘦𝘷𝘦𝘭𝘰𝘱𝘦𝘳 ☘️ :☞︎︎︎ ℝ.𝔻.𝕋𝕙𝕖𝕖𝕜𝕤𝕙𝕒𝕟𝕒 ℍ𝕒𝕟𝕤𝕒𝕞𝕒𝕝 𝕊𝕣𝕚 𝕁𝕒𝕪𝕒𝕝𝕒𝕥𝕙
+
+
+
 */
 
 
@@ -32,25 +28,32 @@ global.APIKeys = {
 }
 
 //other
-global.owner = ['94715166712'] // Owner number , (අයිතිකරුගේ නම්බර් එක )
-global.pemilik = ['94715166712'] //Change  it , ඔබට කැමති නම් වෙනස් කරන්න
-global.premium = ['94715166712'] //Change it ඔබට කැමතිනම් වෙනස් කරන්න 
-global.pengguna = 'Dark Maker' // Your name ඔබේ නම 
-global.botnma = '💃 𝐐𝐮𝐞𝐞𝐧 𝐄𝐥𝐢𝐬𝐚 𝐁𝐨𝐭 💃' // Your bot name , ඔබගෙ බොට්ගේ නම 
-global.ownernma = '𝓓𝓪𝓻𝓴 𝓜𝓪𝓴𝓮𝓻' //ownernama,ownername
-global.packname = 'Queen Elisa Sticker' // Sticker package name 
-global.author = 'Dark Maker' // Sticker Autor name 
+global.owner = ['+94741844543'] // Owner number , (අයිතිකරුගේ නම්බර් එක )
+global.pemilik = ['+94741844543'] //Change  it , ඔබට කැමති නම් වෙනස් කරන්න
+global.premium = ['+94741844543'] //Change it ඔබට කැමතිනම් වෙනස් කරන්න 
+global.pengguna = 'TᕼᗴᗴKՏᕼᗩᑎᗩ ᕼᗩᑎՏᗩᗰᗩᒪ' // Your name ඔබේ නම 
+global.botnma = 'ʕ•ᴥ•ʔ TIKKᗩ ᴡʜᴀᴛsᴀᴘᴘ 𝐁𝐨𝐭 💃' // Your bot name , ඔබගෙ බොට්ගේ නම 
+global.ownernma = '𝓣𝓱𝓮𝓮𝓴𝓼𝓱𝓪𝓷𝓪 𝓗𝓪𝓷𝓼𝓪𝓶𝓪𝓵' //ownernama,ownername
+global.packname = '𝑇𝑖𝑘𝑘𝑎 𝑆𝑡𝑖𝑐𝑘𝑒𝑟 𝑃𝑎𝑐𝑘' // Sticker package name 
+global.author = 'TᕼᗴᗴKՏᕼᗩᑎᗩ' // Sticker Autor name 
 global.sessionName = 'session'
 global.prefa = ['#','!','/',''] 
-global.sp = '🔵'
+global.sp = '◇─────────────────◇
+
+☘️ 𝚍𝚎𝚟𝚎𝚕𝚘𝚙𝚎𝚛 👉:ℝ.𝔻.𝕋𝕙𝕖𝕖𝕜𝕤𝕙𝕒𝕟𝕒 ℍ𝕒𝕟𝕤𝕒𝕞𝕒𝕝 𝕊𝕣𝕚 𝕁𝕒𝕪𝕒𝕝𝕒𝕥𝕙
+
+☘️ 𝚝𝚎𝚕𝚎𝚐𝚛𝚊𝚖 👉: http://t.me/theekshanahansamal
+
+◇─────────────────◇
+𝕚 𝕒𝕞 𝗧𝗜𝗞𝗞𝗔 ᵇᵒᵗ ʕ•ᴥ•ʔ'
 // Alive massage , ඔබේ බොගේ alive massage  එක මෙහි ඇති ` ` මෙම ලකුනු ඇතුලේ ඔබට අවශ්‍ය ඔනිම දෙයක් Type කරන්න.💃
-global.alivelogo = `https://telegra.ph/file/1a2dee71baa975091620d.jpg`
+global.alivelogo = `https://i.ibb.co/6DLCS27/IMG-20220703-234202-052.jpg`
 global.alive =`Hello i am alive now
 
 Download song type .yt 
 Thanks fro using...`
 // Welcome massage  ඔබට කැමති නම් මෙයට ඔබෙ welcome massage  එක යොදන්න 
-global.welcome = `
+global.welcome = `wellcome my whatsapp group`
 🌀 Thanks Fro using sl zim bot
 ✾ Some bugs fixing
 😼♥️
@@ -62,14 +65,16 @@ global.sendgoodbye = `remove`
 // Caption 
 global.cap = `© 𝐆𝐞𝐧𝐞𝐫𝐚𝐭𝐞𝐝 𝐁𝐲 𝐐𝐮𝐞𝐞𝐧 𝐄𝐥𝐢𝐬𝐚 𝐌𝐃`
 global.mess = {
-    success: '✅ Done!',
+    success: '✅ හරි!',
     admin: 'you must be admin to use this cmd!',
     botAdmin: 'Bot must be admin neh!',
     owner: 'This cmd is for Elisa bot owner only',
     group: 'oooh sorry this cmd is for groups only!',
     private: 'goossssh it can only used in dms!',
     bot: 'This cmd  its only for the bot host',
-    wait: 'Wait Elisa bot processing ',
+    wait: 'ටිකක්  ඉන්න 🫥 ',
+ 
+
     endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours',
 }
 global.limitawal = {
